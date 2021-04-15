29dfd60ba94ff6c8b7ff76bc65910962839fd6fc
julia <olkhovyk_ak16@nuwm.edu.ua>
2021-04-15 16:02:07 +0300

    add nodes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -20,9 +20,9 @@ module "eks" {
     {
       name                         = "worker-group"
       instance_type                = "t2.micro"
-      asg_desired_capacity         = 6
+      asg_desired_capacity         = 10
       asg_min_size = 4
-      asg_max_size = 6
+      asg_max_size = 10
       tags = [{
         key = "worker-group-tag"
         value = "worker-group-1"
